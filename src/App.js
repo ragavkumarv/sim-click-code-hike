51009a6b4ac33fac97d176d9d2a70049244b9743
ragavkumarv <ragavkumarv@gmail.com>
2022-03-29 03:44:44 +0530

    code-hike terminal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { sim } from "@code-hike/sim-user";
+import { MiniTerminal } from "@code-hike/mini-terminal";
+import { Terminal } from "../../examples/vite/src/mini-terminal";
+
+import "./styles.css";
+
+// import "../../node_modules/@code-hike/mini-terminal/dist/index.css";
 
 export default function App() {
   const [count, setCount] = React.useState(0);
+
+  const [progress, setProgress] = React.useState(0);
+
+  let slide = 0;
+
+  const yarnCommands = `$ yarn install
+Fetching packages...
+Done.|
+$ yarn dev
+Compiled successfully - ready on http://localhost:3000`;
+
+  useEffect(() => {
+    setInterval(() => {
+      setProgress(slide % yarnCommands.split("|").length);
+      slide++;
+      console.log(
+        slide % yarnCommands.split("|").length,
+        slide,
+        yarnCommands.split("|").length
+      );
+    }, 2000);
+  }, []);
   return (
     <div className="App">
       <h1
         id="inc"
         style={{
           background: "salmon",
-          textAlign: "center"
+          textAlign: "center",
         }}
         onClick={() => setCount(count + 1)}
       >
@@ -22,6 +50,8 @@ export default function App() {
       >
         Simulate Click
       </button>
+
+      <Terminal snippets={yarnCommands.split("|")} index={progress} />
     </div>
   );
 }
